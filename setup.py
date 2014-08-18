6e18c10023db4d2fdfb2d0ba5afab717dcb247b4
Florian Grignon <florian.grignon@ovh.net>
2014-08-18 21:58:40 +0200

    adding setup.py to push to pypi

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-generate-fixtures',
+    version='0.1',
+    description=''' Easy way to generate fixtures (in json)
+    for django following the links of a given object ''',
+    author='aRkadeFR',
+    author_email='contact@arkade.info',
+    url='http://arkadefr.github.io/'
+    packages=['distutils'],
+    )
