16b0fa6d52510eee68d2866a189ee584b916ed6e
aRkadeFR <contact@arkade.info>
2016-07-28 20:15:14 +0200

    Prepare release 0.2

--- setup.py
+++ setup.py
@@ -1,18 +1,7 @@
 # flake8: noqa
-from setuptools import (
-    setup,
-    find_packages,
-)
+from setuptools import setup
 
 setup(
-    name='django-generate-fixtures',
-    version='0.1',
-    description=''' Easy way to generate fixtures (in json)
-    for django following the links of a given object ''',
-    author='aRkadeFR',
-    author_email='contact@arkade.info',
-    url='https://github.com/aRkadeFR/django-generate-fixtures',
-    download_url='https://github.com/arkadefr/django-generate-fixtures/tarball/v0.1',
-    keywords=['django', 'generate', 'fixtures', 'fixture'],
-    packages=find_packages(),
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True,
 )
